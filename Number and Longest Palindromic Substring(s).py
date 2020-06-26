98be95d5c27377f6f6ad4c93b57371d97c2eef9b
Philip Boakye <58409173+fhylinjr@users.noreply.github.com>
2020-06-26 12:11:03 -0700

    Philip Boakye

--- Number and Longest Palindromic Substring(s).py
+++ Number and Longest Palindromic Substring(s).py
@@ -1,14 +1,19 @@
-def palin():
-    S="babaa"
-    N = len(S)
+#This program finds the number of all palindromic substrings.
+#It also outputs the the longest palindromic substring(s) in a given string as parameter.
+#Program considers a single character as palindromic to itself
+# Time complexity on average: O(N^2)
+# Space complexity: O(N)
+
+def palin(word):
+    N = len(word)
     ans = 0
     longest=["0"]
     for center in range(2*N - 1):
         left = center // 2
         right = left + center % 2
-        print(left,right)
-        while left >= 0 and right < N and S[left] == S[right]:
-            long=((S[left:right+1]))
+        #print(left,right)
+        while left >= 0 and right < N and word[left] == word[right]:
+            long=((word[left:right+1]))
             if len(long)>len(longest[0]):
                 longest.append(long)
                 longest.pop(0)
@@ -23,7 +28,7 @@ def palin():
         if len(string)<len(max(longest, key=len)):
             longest.remove(string)
             
-    print(ans)
-    print(longest)
+    print("Number of palindromic substrings: ",ans)
+    print("Logest palindromic substring(s): ",longest)
 
-palin()
+palin("babab")
